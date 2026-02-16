82e2c83440651ced7535c9b1f62ec208f2d4a9ae
WillianServello <rafaelwillian299@gmail.com>
2026-02-16 20:25:08 -0300

    BugCorrigido-ErroSintaxe

--- WpfApp1/obj/Debug/net8.0-windows/UserControls/ucListarProdutos.g.i.cs
+++ WpfApp1/obj/Debug/net8.0-windows/UserControls/ucListarProdutos.g.i.cs
@@ -47,13 +47,13 @@ namespace projetoVendas.UserControls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/projetoVendas;V1.0.0.0;component/usercontrols/uclistarprodutos.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/projetoVendas;component/usercontrols/uclistarprodutos.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\UserControls\ucListarProdutos.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace projetoVendas.UserControls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
