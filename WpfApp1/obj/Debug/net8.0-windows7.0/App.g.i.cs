4b9cb3a513f0cbd0d9972f2229b4336a32be3fa0
WillianServello <rafaelwillian299@gmail.com>
2026-03-26 17:21:50 -0300

    AdicionandoNovoLayout-FaltaBackEnd

--- WpfApp1/obj/Debug/net8.0-windows7.0/App.g.i.cs
+++ WpfApp1/obj/Debug/net8.0-windows7.0/App.g.i.cs
@@ -50,7 +50,7 @@ namespace WpfApp1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -62,7 +62,7 @@ namespace WpfApp1 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/projetoVendas;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/projetoVendas;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -76,7 +76,7 @@ namespace WpfApp1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         public static void Main() {
             WpfApp1.App app = new WpfApp1.App();
             app.InitializeComponent();
